e317fb535853dfa2bcbb96b2ecafbd9169ce1f78
Michał "phoe" Herda <phoe@disroot.org>
2020-05-23 11:29:45 +0200

    Even better dynavars in C

--- 97-dynamic-variables-in-c/dynavars.c
+++ 97-dynamic-variables-in-c/dynavars.c
@@ -1,38 +1,29 @@
 #include <stdio.h>
 
-#define dynamic_name_aux(type, name, postfix)           \
+#define dynamic_name(type, name, postfix)               \
   __dynamic_ ## type ## _ ## name ## _ ## postfix ## __ \
-
-#define dynamic_cleanup_name(type, name) \
-  dynamic_name_aux(type, name, cleanup)  \
-
-#define dynamic_save_name(type, name) \
-  dynamic_name_aux(type, name, save)  \
-
-#define dynamic_continue_name(type, name) \
-  dynamic_name_aux(type, name, continue)  \
 
 #define dynamic_var_aux(type, name, cleanup) \
   void cleanup (type * arg) { name = *arg; } \
 
-#define dynamic_var(type, name, value)              \
-  type name = value;                                \
-  dynamic_var_aux(type,                             \
-                  name,                             \
-                  dynamic_cleanup_name(type, name)) \
+#define dynamic_var(type, name, value)               \
+  type name = value;                                 \
+  dynamic_var_aux(type,                              \
+                  name,                              \
+                  dynamic_name(type, name, cleanup)) \
 
 #define dynamic_bind_aux(type, name, value, save, pop, var)   \
   for(int var = 1; var;)                                      \
     for(type save __attribute__((cleanup(pop))) = name; var;) \
       for(name = value; var; var = 0)                         \
 
-#define dynamic_bind(type, name, value)               \
-  dynamic_bind_aux(type,                              \
-                   name,                              \
-                   value,                             \
-                   dynamic_save_name(type, name),     \
-                   dynamic_cleanup_name(type, name),  \
-                   dynamic_continue_name(type, name)) \
+#define dynamic_bind(type, name, value)                \
+  dynamic_bind_aux(type,                               \
+                   name,                               \
+                   value,                              \
+                   dynamic_name(type, name, save),     \
+                   dynamic_name(type, name, cleanup),  \
+                   dynamic_name(type, name, continue)) \
 
 dynamic_var(int, x, 5);
 
